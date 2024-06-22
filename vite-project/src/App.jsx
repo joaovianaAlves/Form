75c232bd6b02c6545c912a70fb6c8d9234a911d7
joaovianaAlves <joaovitorvianaalves@gmail.com>
2024-06-22 19:31:40 -0300

    Refactor the design

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,14 +6,26 @@ export default function App() {
       <h2 className="font-semibold mb-6 text-2xl text-[#E3FEF7]">
         Cadastro de Produtos:
       </h2>
-      <form className="bg-[#135D66] p-6 rounded-xl shadow-md w-full max-w-md">
+      <form className="bg-[#135D66] p-8 rounded-xl shadow-md w-1/2">
         <InputField text={"Nome"} type={"text"} />
         <InputField text={"Descrição"} type={"text"} />
         <InputField text={"Valor"} type={"number"} />
         <div className="flex items-center mb-4">
-          <span className="mr-4 text-[#E3FEF7]">Disponibilidade:</span>
-          <InputField text={"Sim"} type={"radio"} name={"disponibilidade"} />
-          <InputField text={"Não"} type={"radio"} name={"disponibilidade"} />
+          <span className="mr-4 text-[#E3FEF7]">Disponivel:</span>
+          <div className="flex items-center">
+            <InputField
+              text={"Sim"}
+              type={"radio"}
+              name={"disponibilidade"}
+              id={"sim"}
+            />
+            <InputField
+              text={"Não"}
+              type={"radio"}
+              name={"disponibilidade"}
+              id={"nao"}
+            />
+          </div>
         </div>
         <button
           type="submit"
--- vite-project/src/components/InputField.jsx
+++ vite-project/src/components/InputField.jsx
@@ -1,30 +1,40 @@
 "use Client";
-import React, { useState } from "react";
+import { useState } from "react";
+import "../index.css";
 
+// eslint-disable-next-line react/prop-types
 export default function InputField({ text, type, name, id }) {
   const [value, setValue] = useState("");
   const handleChange = (event) => {
     setValue(event.target.value);
   };
   return type === "radio" ? (
-    <label className="flex items-center mr-4 text-[#E3FEF7]">
+    <div>
       <input
         type={type}
-        value={value}
         id={id}
         name={name}
+        value={value}
         onChange={handleChange}
-        className="mr-1 text-[#E3FEF7] outline-none"
+        className="hidden"
       />
-      {text}
-    </label>
+      <label
+        htmlFor={id}
+        className={`check flex items-center cursor-pointer border p-1 px-2 text-[#E3FEF7] ${
+          id === "sim" ? "rounded-l-2xl" : "rounded-r-2xl"
+        }`}
+      >
+        {text}
+      </label>
+    </div>
   ) : (
-    <div className="flex flex-col">
-      <label className="mb-2 text-[#E3FEF7]"> {text}</label>
+    <div className="relative flex flex-col mb-5">
+      <label className="absolute bottom-11 left-4 mb-2 p-1 text-[#E3FEF7] bg-[#135D66]">
+        {text}
+      </label>
       <input
         type={type}
-        placeholder={text}
-        className="border p-4 rounded-xl mb-2 outline-none"
+        className="border p-4 rounded-xl mb-2 outline-none bg-transparent text-[#E3FEF7]"
         value={value}
         onChange={handleChange}
       />
